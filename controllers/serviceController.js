28f5b6dfba281745ae20cfc430c249f158a095a7
olnbxxx <117682412+olnbxxx@users.noreply.github.com>
2023-08-17 17:49:40 +0200

    dos errores estupidos de palabra y punto y coma

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -4,7 +4,7 @@ const addService = async (req, res) => {
     const { description, price } = req.body;
     const service = new Service({
         description,
-        service
+        price,
     });
     await service.save();
     res.json(service);
@@ -21,6 +21,27 @@ const getServicesById  = async (req, res) => {
     res.json(service);
 };
 
+const editServieById = async (req, res) => {
+    const { serviceId } = req.params;
+    const { description, price } = req.body;
+
+    try {
+        const editedService = await Service.findOneAndUpdate(
+            {_id: serviceId},
+            { $set: {description, price}},
+            {new: true, useFindAndModify: false}
+        );
+
+        if (editedService) {
+            res.json(editedService);
+        } else {
+            res.status(404).json({ message: "No se encontró el servicio con el ID proporcionado." });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error al actualizar el servicio.", error: error.message });
+    }
+};
+
 const deleteServiceById = async (req, res) => {
     const { serviceId } = req.params;
     await Service.deleteOne({_id: serviceId });
@@ -29,5 +50,6 @@ const deleteServiceById = async (req, res) => {
 
 module.exports = {
     addService, getService,
+    editServieById,
     getServicesById, deleteServiceById
 }
--- routes/serviceRoute.js
+++ routes/serviceRoute.js
@@ -9,6 +9,8 @@ router.get("/", serviceController.getService);
 
 router.get("/:serviceId", serviceController.getServicesById);
 
+router.patch("/:serviceId", serviceController.editServieById);
+
 router.delete("/:serviceId", serviceController.deleteServiceById);
 
 module.exports = router;
