b651ec8d62696da88f6471ecb6d186c35b9b7066
olnbxxx <117682412+olnbxxx@users.noreply.github.com>
2023-08-26 11:11:25 +0200

    payment terminado

--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -7,4 +7,12 @@ router.post("/",paymentController.assingService);
 
 router.get("/", paymentController.getPayments);
 
+router.get("/", paymentController.getPaymentById);
+
+router.delete("/", paymentController.deletePayment);
+
+router.get("/", paymentController.getServicesByCustomerId);
+
+router.get("/", paymentController.getPaymentByCustomerId);
+
 module.exports = router;
